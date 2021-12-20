4b5911487747e138a5eac52e132cae166f32d7de
Wladimir Sepulveda <wasepulvedac@gmail.com>
2021-12-20 17:33:07 -0300

    lol

--- src/routes/invoices.jsx
+++ src/routes/invoices.jsx
@@ -1,20 +1,19 @@
-import { Link } from 'react-router-dom';
-import { getInvoices } from '../data';
+import { Link, Outlet } from "react-router-dom";
+import { getInvoices } from "../data";
 
 export default function Invoices() {
   let invoices = getInvoices();
-  console.log('invoices', invoices);
   return (
-    <div style={{ display: 'flex' }}>
+    <div style={{ display: "flex" }}>
       <nav
         style={{
-          borderRight: 'solid 1px',
-          padding: '1rem',
+          borderRight: "solid 1px",
+          padding: "1rem"
         }}
       >
-        {invoices.map((invoice) => (
+        {invoices.map(invoice => (
           <Link
-            style={{ display: 'block', margin: '1rem 0' }}
+            style={{ display: "block", margin: "1rem 0" }}
             to={`/invoices/${invoice.number}`}
             key={invoice.number}
           >
@@ -22,6 +21,7 @@ export default function Invoices() {
           </Link>
         ))}
       </nav>
+      <Outlet />
     </div>
   );
 }
